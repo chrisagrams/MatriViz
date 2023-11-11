0b770e6bc58b5164748e97586a02e32e1a19fea6
Chris Grams <chrisagrams@gmail.com>
2023-11-10 20:07:03 -0600

    cleaned up lasso

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react'
 import { Group } from '@visx/group'
 import { Circle } from '@visx/shape'
 import { scaleLinear } from '@visx/scale'
-import { line } from 'd3-shape'
+import { LinearGradient } from '@visx/gradient'
+import { line, curveBasis } from 'd3-shape'
 import { polygonContains } from 'd3-polygon'
 
 const App = (): JSX.Element => {
-  const [data, setData] = useState([]) // State to store the data
-  const [lassoPoints, setLassoPoints] = useState([])
+  const [data, setData] = useState([])
+  const [dimensions, setDimensions] = useState({
+    width: window.innerHeight - 50,
+    height: window.innerHeight - 50
+  })
+  const [lassoPoints, setLassoPoints] = useState<[number, number][]>([])
   const [selectedPoints, setSelectedPoints] = useState([])
   const [isLassoActive, setIsLassoActive] = useState(false)
 
@@ -31,46 +36,70 @@ const App = (): JSX.Element => {
       })
   }, [])
 
-  const width = 600
-  const height = 600
+  useEffect(() => {
+    const handleResize = (): void => {
+      setDimensions({ width: window.innerHeight - 50, height: window.innerHeight - 50 })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const xScale = scaleLinear({
-    range: [0, width],
+    range: [0, dimensions.width],
     domain: [Math.min(...data.map((d) => d.x)), Math.max(...data.map((d) => d.x))]
   })
   const yScale = scaleLinear({
-    range: [height, 0],
+    range: [dimensions.height, 0],
     domain: [Math.min(...data.map((d) => d.y)), Math.max(...data.map((d) => d.y))]
   })
 
-  const handleMouseDown = (event) => {
+  const handleMouseDown = (event: React.MouseEvent): void => {
     setIsLassoActive(true)
     setLassoPoints([[event.clientX, event.clientY]])
   }
 
-  const handleMouseMove = (event) => {
+  const handleMouseMove = (event: React.MouseEvent): void => {
     if (!isLassoActive) return
-    setLassoPoints([...lassoPoints, [event.clientX, event.clientY]])
+
+    const updatedLassoPoints: [number, number][] =
+      lassoPoints.length > 1 ? lassoPoints.slice(0, -1) : lassoPoints
+
+    const newPoint: [number, number] = [event.clientX, event.clientY]
+
+    const newPoints: [number, number][] = [...updatedLassoPoints, newPoint]
+
+    // Temporarily close the loop for visual effect
+    if (newPoints.length > 1) {
+      newPoints.push(newPoints[0])
+    }
+
+    setLassoPoints(newPoints)
   }
 
-  const handleMouseUp = () => {
+  const handleMouseUp = (): void => {
     setIsLassoActive(false)
     const selected = data.filter((d) => polygonContains(lassoPoints, [xScale(d.x), yScale(d.y)]))
     setSelectedPoints(selected)
+    console.log('Selected points:', selected)
   }
 
-  const lassoPath = line()(lassoPoints)
+  const lassoPath = line().curve(curveBasis)(lassoPoints)
 
   return (
     <div className="container">
       <svg
-        width={width}
-        height={height}
+        width={dimensions.width}
+        height={dimensions.height}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
       >
-        <rect width={width} height={height} fill="transparent" />
+        {/* <rect width={dimensions.width} height={dimensions.height} fill="transparent" /> */}
+        <LinearGradient id="stroke" from="#ff614e" to="#ffdc64" />
         <Group>
           {data.map((point, i) => (
             <Circle
@@ -82,7 +111,7 @@ const App = (): JSX.Element => {
             />
           ))}
         </Group>
-        {isLassoActive && <path d={lassoPath} fill="none" stroke="blue" />}
+        {isLassoActive && <path d={lassoPath} fill="none" stroke="url(#stroke)" strokeWidth={3} />}
       </svg>
     </div>
   )
